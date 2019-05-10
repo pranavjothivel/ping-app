bc08f66ad5b141b332a0186ca9b58f41feef826f
Pranav Jothivel <pranav.jothivel@outlook.com>
2019-05-10 21:05:27 +0000

    Initial commit

--- __init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, jsonify
-
-app = Flask(__name__)
-
-@app.route('/api/ping')
-def ping():
-    return jsonify({'ping': 'pong'})
--- wsgi.py
+++
@@ -1,4 +0,0 @@
-import ping-app
-
-if __name__ == "__main__":
-    app.run()
